668e6b91f503f647f2d6601d85168240b2d8cc57
Mustafa Kan <79532179+mustafakan01@users.noreply.github.com>
2023-12-08 20:53:33 +0300

    Final

--- ReactProje/src/App.jsx
+++ ReactProje/src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import AddItem from './AddItem'
 
 function App() {
   const [newitem, setnewitem] = useState("")
